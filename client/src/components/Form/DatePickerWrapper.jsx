487dd490be9a2094166ec5ef60a8f7ce317a6593
Omri Kornblau <omri.354@gmail.com>
2020-05-28 01:26:52 +0300

    Add form body component

---
+++ client/src/components/Form/DatePickerWrapper.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+  DatePicker
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
+const propTypes = {
+  label: PropTypes.string,
+  format: PropTypes.string
+}
+
+const defaultProps = {
+  format: "MM/dd/yyyy"
+}
+
+const DatePickerWrapper = props => {
+  const {
+    label,
+    format
+  } = props;
+
+  return <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <KeyboardDatePicker
+      disableToolbar
+      variant="inline"
+      format={format}
+      margin="normal"
+      id="date-picker-inline"
+      label={label}
+      KeyboardButtonProps={{
+        'aria-label': 'change date',
+      }}
+    />
+  </MuiPickersUtilsProvider>
+}
+
+DatePickerWrapper.propTypes = propTypes;
+DatePickerWrapper.defaultProps = defaultProps;
+
+export default DatePickerWrapper;
---
+++ client/src/components/Form/FormBody.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Grid
+} from "@material-ui/core";
+
+import DatePickerWrapper from "./DatePickerWrapper";
+import TextFieldWrapper from "./TextFieldWrapper";
+
+const Input = props => {
+  switch(props.type) {
+    case "date":
+      return <DatePickerWrapper {...props}/>
+
+    default:
+      return <TextFieldWrapper {...props}/>
+  }
+}
+
+const propTypes = {
+  formStructure: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.objectOf({
+        label: PropTypes.string,
+        helperText: PropTypes.string,
+        defaultValue: PropTypes.string
+      })
+    )
+  ),
+  variant: PropTypes.oneOf(["outlined", "filled", "standart"]),
+  spacing: PropTypes.number,
+  margin: PropTypes.oneOf(["dense", "none", "normal"]),
+  dateFormat: PropTypes.string,
+  justify: PropTypes.oneOf(["flex-start", "flex-end", "center"])
+}
+
+const defaultProps = {
+  formStructure: [[]],
+  dateFormat: "MM/dd/yyyy",
+  justify: "flex-start"
+}
+
+const FormBody = props => {
+  const {
+    formStructure,
+    variant,
+    spacing,
+    margin,
+    dateFormat,
+    justify
+  } = props;
+
+  return (
+    formStructure.map(row =>
+      <Grid container spacing={spacing} direction="row">
+        {row.map(field =>
+          <Grid xs item container justify={field.justify || justify}>
+            <Input
+              variant={field.variant || variant}
+              margin={margin}
+              format={dateFormat}
+              type={field.type}
+              label={field.label}
+              defaultValue={field.defaultValue}
+              helperText={field.helperText}
+              fullWidth={field.fullWidth}
+              required={field.required}
+            />
+          </Grid>
+        )}
+      </Grid>
+    )
+  );
+}
+
+FormBody.propTypes = propTypes;
+FormBody.defaultProps = defaultProps;
+
+export default FormBody;
