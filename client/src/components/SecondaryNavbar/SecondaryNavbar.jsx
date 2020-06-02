ef13b80ffc5c339f3d8926b6a671af379090517b
Omri Kornblau <omri.354@gmail.com>
2020-06-02 23:53:19 +0300

    Add new request view

--- client/src/components/SecondaryNavbar/SecondaryNavbar.jsx
+++ client/src/components/SecondaryNavbar/SecondaryNavbar.jsx
@@ -7,9 +7,10 @@ import {
   Toolbar,
   Tabs,
   Tab,
-  IconButton,
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
-
+import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
 import { Link } from "react-router-dom";
 
 import useStyle from "./style";
@@ -26,26 +27,57 @@ const SecondaryNavbar = props => {
   } = props;
 
   const [tab, setTab] = useState(0);
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const classes = useStyle();
 
   useEffect(() => {
     setTab(_.keys(views).indexOf(viewKey));
   }, [viewKey]);
 
+  const onNewRequestClick = e => {
+    setMenuAnchorEl(e.currentTarget);
+  }
+
+  const onCloseNewRequestMenu = () => {
+    setMenuAnchorEl(null);
+  }
+
+  const newRequestKey = "newrequest";
+
   return (
-    <Toolbar variant="dense" className={classes.toolbar}>
+    <Toolbar className={classes.toolbar}>
       <Tabs
         value={tab}
         indicatorColor="primary"
       >
+        <Menu
+          id="simple-menu"
+          anchorEl={menuAnchorEl}
+          keepMounted
+          open={!!menuAnchorEl}
+          onClose={onCloseNewRequestMenu}
+        >
+          <Link to={newRequestKey}>
+            <MenuItem onClick={onCloseNewRequestMenu}>ביטוח א'</MenuItem>
+            <MenuItem onClick={onCloseNewRequestMenu}>ביטוח ב'</MenuItem>
+            <MenuItem onClick={onCloseNewRequestMenu}>ביטוח ג'</MenuItem>
+          </Link>
+        </Menu>
         {_.map(views, (viewData, key) =>
-          <Tab
-            label={viewData.name}
-            component={Link}
-            to={`${key}`}
-            icon={<viewData.icon/>}
-            key={key}
-          />
+          key === newRequestKey ?
+            <Tab
+              label={viewData.name}
+              icon={<viewData.icon/>}
+              onClick={onNewRequestClick}
+              key={key}
+            />
+            : <Tab
+              label={viewData.name}
+              component={Link}
+              to={`${key}`}
+              icon={<viewData.icon/>}
+              key={key}
+            />
         )}
       </Tabs>
     </Toolbar>
